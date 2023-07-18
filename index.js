4a432ad9fc7ffea3bcb1a53e599b3186b809ccd0
Adedamola Ajasa <azeemajasa@gmail.com>
2023-07-18 09:22:08 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const name = "Susan";
-const height = 35;
+const name = "Joe";
+const height = 74;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
-
 
